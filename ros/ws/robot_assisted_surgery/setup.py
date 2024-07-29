9d0b8a14ec0e77dc24b3cdab7b188860c7498f15
kassidyshedd <kass92800@gmail.com>
2024-07-29 16:36:37 -0500

    Created working camera node, launch file

--- ros/ws/robot_assisted_surgery/setup.py
+++ ros/ws/robot_assisted_surgery/setup.py
@@ -9,7 +9,9 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['package.xml', 
+                                   'launch/robot_surgery.launch.py']),
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +22,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'camera_node = robot_assisted_surgery.camera_node:main'
         ],
     },
 )
